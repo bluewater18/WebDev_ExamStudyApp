b152b662573bb859ba1f2349b8eb833b63e08fb9
Morgan English <mme88@uclive.ac.nz>
2018-11-14 18:25:09 +1300

    added a material ui card to the help page and played around with css to get it in the center of the screen. Still need to work on content and sizing

--- exam-study-app/src/scripts/components/Help.jsx
+++ exam-study-app/src/scripts/components/Help.jsx
@@ -1,16 +1,59 @@
 ﻿import React from 'react';
+import PropTypes from 'prop-types';
+import { Card, CardActions, CardContent, CardHeader, Typography, Button, withStyles, Grid } from '@material-ui/core'
 
+const styles = {
+    card: {
+        minWidth: 275,
+        maxWidth: 300,
+        maxHeight: 500,
+        float: "center"
+        
+    },
+    bullet: {
+        display: 'inline-block',
+        margin: '0 2px',
+        transform: 'scale(0.8)',
+    },
+    title: {
+        fontSize: 14,
+    },
+    pos: {
+        marginBottom: 12,
+    },
+};
 
 class Help extends React.Component {
     render() {
+        const { classes } = this.props;
         return (
-            <div className="Help">
+            <div className="Help" >
                 <div className="Hero-Background" />
-                <h1 style={{ color: "black" }}>Exam Study Web Application Help Page</h1>
-                <hr/>
+                    <Card className="Help-Card">
+                        <CardContent>
+                            <Typography variant="h5" component="h2">
+                                About
+                                </Typography>
+                            <Typography className={classes.pos} color="textSecondary">
+                                adjective
+                                </Typography>
+                            <Typography component="p">
+                                well meaning and kindly.
+                                <br />
+                                {'"a benevolent smile"'}
+                            </Typography>
+                        </CardContent>
+                        <CardActions>
+                            <Button size="small">Learn More</Button>
+                        </CardActions>
+                    </Card>
             </div>
         );
     }
 }
 
-export default Help;
+Help.PropType = {
+    classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Help);
