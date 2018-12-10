597b31629daff165953671680e6cf2a7d0122ed8
Morgan English <mme88@uclive.ac.nz>
2018-12-10 22:39:34 +1300

    more saga and api functionality for questions

--- exam-study-app/src/scripts/api-calls/api-questions.js
+++ exam-study-app/src/scripts/api-calls/api-questions.js
@@ -1,5 +1,6 @@
 import { createFetch, base, method, header, body } from 'http-client';
 import { API_BASE_PATH } from '../constants/index';
+import { promises } from 'fs';
 
 async function apiAddQuestion(question) {
     return new Promise((resolve, reject) => {
@@ -43,5 +44,53 @@ async function apiEditQuestion(question) {
 
 }
 
+async function apiDeleteQuestion( questionId) {
+    return new Promise((resolve, reject) => {
+        const fetchDeleteQuestion =  createFetch(
+            base(API_BASE_PATH),
+            method("DELETE"),
+        )
+        fetchDeleteQuestion('/resource/question/'+questionId).then((res) => {
+            if(res.status === 200)
+                resolve()
+            else 
+                reject(res)
+        }).catch((err) =>
+            reject(err))
+    })
+}
 
-export {apiAddQuestion, apiEditQuestion};
+async function apiUpvoteQuestion(questionId) {
+    return new Promise((resolve, reject) => {
+        const fetchUpvoteQuestion = createFetch(
+            base(API_BASE_PATH),
+            method("POST"),
+        )
+        fetchUpvoteQuestion('/resource/question/upvote/'+questionId).then((res) => {
+            if(res.status === 200)
+                resolve()
+            else
+                reject(res)
+        }).catch((err) => 
+            reject(err))
+    })  
+}
+
+
+async function apiDownvoteQuestion(questionId) {
+    return new Promise((resolve, reject) => {
+        const fetchDownvoteQuestion = createFetch(
+            base(API_BASE_PATH),
+            method("POST"),
+        )
+        fetchDownvoteQuestion('/resource/question/downvote/'+questionId).then((res) => {
+            if(res.status === 200)
+                resolve()
+            else
+                reject(res)
+        }).catch((err) => 
+            reject(err))
+    })  
+}
+
+export {apiAddQuestion, apiEditQuestion, apiDeleteQuestion, apiUpvoteQuestion, apiDownvoteQuestion};
--- exam-study-app/src/scripts/sagas/saga-questions.js
+++ exam-study-app/src/scripts/sagas/saga-questions.js
@@ -1,6 +1,6 @@
-﻿import { apiAddQuestion } from '../api-calls/api-questions';
+﻿import { apiAddQuestion, apiEditQuestion, apiDeleteQuestion, apiUpvoteQuestion, apiDownvoteQuestion } from '../api-calls/api-questions';
 import { actionConstants } from '../constants/index';
-import { call, all, put, takeLatest } from 'redux-saga/effects';
+import { call, all, put, takeLatest, } from 'redux-saga/effects';
 
 function* addQuestion({payload}) {
     try {
@@ -15,13 +15,62 @@ function* addQuestion({payload}) {
     }
 }
 
+function* deleteQuestion({payload}) {
+    try{
+        let questionId = payload;
+        yield call(apiDeleteQuestion, questionId)
+        yield put({ type: actionConstants.DELETE_QUESTION_SUCCESS, payload: questionId })
+    } catch (err) {
+        console.log(err)
+        yield put({ type: actionConstants.DELETE_QUESTION_FAILURE })
+    }
+}
+
+function* editQuestion({payload}) {
+    try {
+        let question = payload;
+        yield call(apiEditQuestion, question)
+        yield put({type:actionConstants.EDIT_QUESTION_SUCCESS})
+    } catch(err) {
+        console.log(err)
+        yield put({type: actionConstants.EDIT_QUESTION_FAILURE})
+    } 
+}
+
+function* upvoteQuestion({payload}) {
+    try{
+        let questionId = payload.questionId;
+        let userId = payload.userId;
+        yield call(apiUpvoteQuestion, questionId, userId)
+        yield put({type: actionConstants.UPVOTE_QUESTION_SUCCESS})
+    } catch (err) {
+        console.log(err)
+        yield put({type: actionConstants.UPVOTE_QUESTION_FAILURE})
+    }
+}
+
+function* downvoteQuestion({payload}) {
+    try{
+        let questionId = payload.questionId;
+        let userId = payload.userId;
+        yield call(apiDownvoteQuestion, questionId, userId)
+        yield put({type: actionConstants.DOWNVOTE_QUESTION_SUCCESS})
+    } catch (err) {
+        console.log(err)
+        yield put({type: actionConstants.DOWNVOTE_QUESTION_FAILURE})
+    }
+}
+
 
 
 
 export default function* root() {
     yield all([
         takeLatest(actionConstants.ADD_QUESTION, addQuestion),
-
+        takeLatest(actionConstants.EDIT_QUESTION, editQuestion),
+        takeLatest(actionConstants.deleteQuestion, deleteQuestion),
+        takeLatest(actionConstants.UPVOTE_QUESTION, upvoteQuestion),
+        takeLatest(actionConstants.DOWNVOTE_QUESTION, downvoteQuestion)
 
     ]);
 }
